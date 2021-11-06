6026a01b8c9c7b350abc8eccdd70825a268a4bac
CRUVOD <haoran0088@hotmail.com>
2021-11-06 17:03:34 -0400

    Deeper Integration

--- Tests/app/Test2.py
+++ Tests/app/Test2.py
@@ -7,8 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    name = 'Rosalia'
-    return render_template('index.html', title='Welcome', username=name)
+    content1 = 'Lorem'
+    content2 = 'Body'
+    dates = ['12/1',
+                '2/1',
+                '7/4',
+                '8/4'
+                ]
+    return render_template('index.html', 
+                           title='Welcome', 
+                           dates=dates,
+                           content1=content1,
+                           content2=content2)
 
 app.run(host='0.0.0.0', port=81)
 
