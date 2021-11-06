55f7d77467312d3f4a330cdb09bbd5c1998fdf61
CRUVOD <haoran0088@hotmail.com>
2021-11-06 19:39:14 -0400

    Added Test JSON file

--- Tests/app/Test2.py
+++ Tests/app/Test2.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 
@@ -7,13 +7,25 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    content1 = 'Lorem'
-    content2 = 'Body'
+    content1 = ['']
+    content2 = ['']
+    dates = ['']
+    return render_template('index.html', 
+                           title='Welcome', 
+                           dates=dates,
+                           content1=content1,
+                           content2=content2)
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.strip()
+    content1 = [processed_text,'title2','title3','title4']
+    content2 = ['Body1','Body2','Body3','Body4']
     dates = ['12/1',
                 '2/1',
                 '7/4',
-                '8/4'
-                ]
+                '8/4']
     return render_template('index.html', 
                            title='Welcome', 
                            dates=dates,
