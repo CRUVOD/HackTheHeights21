efb1463b09b4c79775d3bd03767a64eb21cb58ab
seminyoon1 <seminyoon1@gmail.com>
2021-11-06 14:50:38 -0400

    Timeline loop now works

--- sample.js
+++ sample.js
@@ -1,4 +1,10 @@
-<script>
-const element = document.getElementById("div1");
-element.appendChild(para);
-</script>
+document.addEventListener('DOMContentLoaded', function() {
+    for (let i = 0; i < 5; i++) {
+        var div = document.createElement('div');
+        div.id = 'container';
+        div.innerHTML = 'Hi there!';
+        div.className = 'border pad';
+        
+        document.body.appendChild(div);
+      }
+}, false);
