c83a0a8cba6e09b55db4bed1c0e919e0f5ef3b12
seminyoon1 <seminyoon1@gmail.com>
2021-11-06 17:22:54 -0400

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,11 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
-    for (let i = 0; i < 5; i++) {
-        var div = document.createElement('div');
-        div.id = 'container';
-        div.innerHTML = 'Hi there!';
-        div.className = 'border pad';
-        
-        //document.body.appendChild(div);
-        document.body.appendChild(div);
-      }
-}, false);
+        var current = document.querySelector('.container.right');
+        for(let i = 0; i < 4; i++) {
+          if(i % 2 == 0) {
+            var containerleft = document.createElement('div');
+            containerleft.className = 'container left';
+            current.insertAdjacentElement('afterend', containerleft);
+            current = containerleft;
+
+            var dataleft = document.createElement('div');
+            dataleft.innerHTML = 'some text';
+            dataleft.className = 'dataleft';
+            current.append(dataleft);
+
+            var icon = document.createElement('i');
+            icon.className = 'icon fa fa-home';
+            current.append(icon);
+
+            var content = document.createElement('div');
+            content.className = 'content';
+            current.append(content);
+
+            var h2 = document.createElement('h2');
+            h2.innerHTML = 'text here';
+            content.append(h2);
+
+            var p = document.createElement('p');
+            p.innerHTML = 'paragraph here';
+            content.append(p);
+          } else {
+            var containerright = document.createElement('div');
+            containerright.className = 'container right';
+            current.insertAdjacentElement('afterend', containerright);
+            current = containerright;
+
+            var dataright = document.createElement('div');
+            dataright.innerHTML = 'some text';
+            dataright.className = 'dataright';
+            current.append(dataright);
+
+            var icon = document.createElement('i');
+            icon.className = 'icon fa fa-gift';
+            current.append(icon);
+
+            var content = document.createElement('div');
+            content.className = 'content';
+            current.append(content);
+
+            var h2 = document.createElement('h2');
+            h2.innerHTML = 'text here';
+            content.append(h2);
+
+            var p = document.createElement('p');
+            p.innerHTML = 'paragraph here';
+            content.append(p);
+          }
+        }
+      }, false);
